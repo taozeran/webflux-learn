930654c1da960e288eb1ff926ba34d2d78491cc9
taozeran <taozeran@show.one>
2019-03-26 20:09:18 +0800

    add discard server by netty

--- src/main/java/com/tao/webfluxlearn/utils/DiscardServerHandler.java
+++ src/main/java/com/tao/webfluxlearn/utils/DiscardServerHandler.java
@@ -1,17 +1,27 @@
 package com.tao.webfluxlearn.utils;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.concurrent.EventExecutorGroup;
 
-public class DiscardServerHandler implements ChannelHandler{
-    @Override
-    public void handlerAdded(ChannelHandlerContext channelHandlerContext) throws Exception {
-
-    }
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
 
     @Override
-    public void handlerRemoved(ChannelHandlerContext channelHandlerContext) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        }finally {
+
+        }
+
+        System.out.println("a new client in = "+ctx.name());
 
     }
 }
